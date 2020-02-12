23e581c81f70c1319348a9d268eee04ce4c57376
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-12 13:40:05 -0500

    Fixes bug where no letter shows at end of alphabet

--- src/reducers.js
+++ src/reducers.js
@@ -3,7 +3,7 @@ import { NEXT, LAST, RESET } from "./actions";
 
 const defaultState = {
     characters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    position: 13
+    position: 12
 };
 
 function letterCount(state=defaultState, action) {
@@ -11,13 +11,13 @@ function letterCount(state=defaultState, action) {
 
     switch (action.type) {
         case NEXT:
-            ++newState.position;
+            newState.position >= 25 ? newState.position = 0 : ++newState.position;
             break;
         case LAST:
-            --newState.position;
+            newState.position <= 0 ? newState.position = 25 : --newState.position;
             break;
         case RESET:
-            newState.position = 13;
+            newState.position = 12;
             break;
         default:
             break;
