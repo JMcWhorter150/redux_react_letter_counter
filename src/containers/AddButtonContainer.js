a4af4134b172d655cae031ca91fad4e3879d00d5
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-13 09:26:21 -0500

    Removes mapstatetoprops from addbutton

--- src/containers/AddButtonContainer.js
+++ src/containers/AddButtonContainer.js
@@ -2,16 +2,12 @@ import {connect} from 'react-redux';
 import AddButton from '../components/AddButton';
 import {actionAddButton} from '../actions';
 
-function mapStateToProps(state) {
-    return {};
-}
-
 function mapDispatchToProps(dispatch) {
     return {
         handleAddButton: () => {dispatch(actionAddButton())}
     }
 }
 
-const reduxConnector = connect(mapStateToProps, mapDispatchToProps);
+const reduxConnector = connect(null, mapDispatchToProps);
 
 export default reduxConnector(AddButton);
